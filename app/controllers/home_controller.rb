0569d0751973ca96db3465f600ed143dfd31a015
AxelVee <axel_vee@hotmail.it>
2012-03-17 11:53:41 +0100

    cambio nome sito

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,19 @@
 class HomeController < ApplicationController
+
+  def home
+    @title = "Home"
+  end
+  
+  def contact
+    @title = "Contact us"
+  end
+  
+  def about
+    @title = "About"
+  end
+
+
+
   def users
   end
 
