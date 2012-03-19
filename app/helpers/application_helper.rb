25cffcc5e26f1c77d06a5f1514e2a8f1e898880b
AxelVee <axel_vee@hotmail.it>
2012-03-19 04:13:42 +0100

    prova2

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  
+  #return un nome diverso per ogni pagina
+  
+  def title
+    base_title = "La Gilda"
+    if @title.nil?
+    base_title
+    else
+      "#{base_title} | #{@title}"    
+      end
+  end
 end
