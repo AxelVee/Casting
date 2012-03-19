6f2cb2d632f1ed7215c26d7272ec6aa784220e7b
AxelVee <axel_vee@hotmail.it>
2012-03-19 05:12:06 +0100

    prova2

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,10 +4,10 @@ module ApplicationHelper
   
   def title
     base_title = "La Gilda"
-    if @title.nil
+    if @title.nil?
       base_title
     else
-      "#{base_title} | #{@title}"    
-      end
+      "#{base_title} | #{@title}"
+    end
   end
 end
